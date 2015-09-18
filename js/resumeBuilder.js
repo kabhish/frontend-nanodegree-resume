d82b674fa23ad17f28b6864e262fc084486ecc92
kabhish <k.abhishek70@gmail.com>
2015-09-18 23:24:49 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,4 +4,4 @@ This is empty on purpose! Your code to build the resume will go here.
  	//$("#main").append("Abhishek");
  var awesomeThoughts = "I am Abhishek and I am AWESOME";
  var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
- .append(funThoughts);
+ $("#main").append(funThoughts);
